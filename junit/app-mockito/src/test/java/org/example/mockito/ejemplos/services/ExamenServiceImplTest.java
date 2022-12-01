faed7628349ba583d93af63872db5695f3385496
Jonathan <jonathanoleaz@hotmail.com>
2022-11-30 20:49:35 -0600

    Se agrega ejemplo con anotaciones de Mockito para no crear los mock manualmente, tambien se agrega ejemplo de verify

--- junit/app-mockito/src/test/java/org/example/mockito/ejemplos/services/ExamenServiceImplTest.java
+++ junit/app-mockito/src/test/java/org/example/mockito/ejemplos/services/ExamenServiceImplTest.java
@@ -6,7 +6,10 @@ import org.example.mockito.ejemplos.repositories.ExamenRepositoryImpl;
 import org.example.mockito.ejemplos.repositories.PreguntaRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -15,28 +18,32 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class ExamenServiceImplTest {
-
+    //Con la anotacion @Mock se crean como mocks
+    @Mock
     ExamenRepository repository;
-    ExamenService service;
+    @Mock
     PreguntaRepository preguntaRepository;
+    //Con la anotacion @InjectMocks se inyectac los mocks
+    @InjectMocks
+    ExamenServiceImpl service;
 
     @BeforeEach
     void setUp(){
+        //Habilitamos el uso de anotaciones de Mockito
+        MockitoAnnotations.openMocks(this);
         //Creamos una simulacion al vuelo de ExamenRepository, solo se puede hacer mock de los metodos publicos o default en el mismo package
-         repository = Mockito.mock(ExamenRepositoryImpl.class); //new ExamenRepositoryImpl();
-        preguntaRepository = mock(PreguntaRepository.class);
-         service = new ExamenServiceImpl(repository, preguntaRepository);
+        //repository = Mockito.mock(ExamenRepositoryImpl.class); //new ExamenRepositoryImpl();
+        //preguntaRepository = mock(PreguntaRepository.class);
+        //service = new ExamenServiceImpl(repository, preguntaRepository);
     }
 
 
     @Test
     void findExamenPorNombre() {
-
-
+        //Mockeamos o simulamos los repositorios que proporcionan datos a los services
         when(repository.findAll()).thenReturn(Datos.EXAMENES);
 
         Optional<Examen> examen= service.findExamenPorNombre("Matematicas");
@@ -51,6 +58,7 @@ class ExamenServiceImplTest {
         //Creamos una simulacion al vuelo de ExamenRepository, solo se puede hacer mock de los metodos publicos o default en el mismo package
         List<Examen> datos = Collections.emptyList();
 
+        //Mockeamos o simulamos los repositorios que proporcionan datos a los services
         when(repository.findAll()).thenReturn(datos);
 
         Optional<Examen> examen= service.findExamenPorNombre("Matematicas");
@@ -60,11 +68,41 @@ class ExamenServiceImplTest {
 
     @Test
     void testPreguntasExamen() {
+        //Mockeamos o simulamos los repositorios que proporcionan datos a los services
         when(repository.findAll()).thenReturn(Datos.EXAMENES);
         when(preguntaRepository.findPreguntasPorExamenId(anyLong())).thenReturn(Datos.PREGUNTAS);
         Examen examen = service.findExamenPorNombreConPreguntas("Historia");
 
         assertEquals(5, examen.getPreguntas().size());
         assertTrue(examen.getPreguntas().contains("aritmetica"));
+    }
+
+    @Test
+    void testPreguntasExamenVerify() {
+        //Mockeamos o simulamos los repositorios que proporcionan datos a los services
+        when(repository.findAll()).thenReturn(Datos.EXAMENES);
+        when(preguntaRepository.findPreguntasPorExamenId(anyLong())).thenReturn(Datos.PREGUNTAS);
+        Examen examen = service.findExamenPorNombreConPreguntas("Matematicas");
+
+        assertEquals(5, examen.getPreguntas().size());
+        assertTrue(examen.getPreguntas().contains("aritmetica"));
+
+        //Verificamos que en repository se invoque el metodo findAll
+        verify(repository).findAll();
+        verify(preguntaRepository).findPreguntasPorExamenId(5L);
+    }
+
+    @Test
+    void testPretestNoExisteExamenVerify() {
+        //Mockeamos o simulamos los repositorios que proporcionan datos a los services
+        when(repository.findAll()).thenReturn(Collections.emptyList());
+        when(preguntaRepository.findPreguntasPorExamenId(anyLong())).thenReturn(Datos.PREGUNTAS);
+        Examen examen = service.findExamenPorNombreConPreguntas("Matematicas");
+
+        assertNull(examen);
+
+        //Verificamos que en repository se invoque el metodo findAll
+        verify(repository).findAll();
+        //verify(preguntaRepository).findPreguntasPorExamenId(5L);
     }
 }
